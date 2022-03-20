03781c56078fd302f009ee2a233eb1b5b1d88ae3
Charlie Milano <charmilano@gmail.com>
2022-03-20 15:36:17 -0700

    got the background-img and new font in CSS

--- js/app.js
+++ js/app.js
@@ -1,8 +1,18 @@
 // Make class 'Birds' as BUTTONS and have them appear 
 //and disappear from screen
 
-class Birds {
-    constructor(){
-        
-    }
-};
+//Play button to start the game. Reset the score and restart timer.
+
+//ti
+
+let score; //score based on button clicks
+let timer; //timer that starts @ 20 seconds and counts down
+
+
+const madeShot = document.querySelector('#score');
+const replayBtn = document.querySelector('#play"')
+
+
+//setup event listeners
+
+document.querySelector('#play').addEventListener('click', handleClick);
