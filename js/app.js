fbea9dbe01dc68229fe4cc466d9295d64b4b557d
Charlie Milano <charmilano@gmail.com>
2022-03-21 18:50:54 -0700

    working with Ryan have new divs

--- js/app.js
+++ js/app.js
@@ -3,16 +3,108 @@
 
 //Play button to start the game. Reset the score and restart timer.
 
-//ti
+//global variables
+const score = 0;
+const timer = 30;
+const winningScore = 10;
+const birds = document.querySelector('#clickable');
+const playBtn = document.querySelector('#play');
+const bottomEls = document.querySelector('#timer');
+const bottomEltwo = document.querySelector('#score');
 
-let score; //score based on button clicks
-let timer; //timer that starts @ 20 seconds and counts down
 
 
-const madeShot = document.querySelector('#score');
-const replayBtn = document.querySelector('#play"')
+
+// Function that runs the timer////////
+function startTimer(duration, display) {
+    let timer = duration, seconds;
+    setInterval(function () {
+        
+        seconds = parseInt(timer % 2, 10);
+
+        
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = ":" + seconds;
+
+        if (--timer < 0) {
+            timer = 0; 
+            
+        }
+    }, 1000);
+}
 
 
-//setup event listeners
 
-document.querySelector('#play').addEventListener('click', handleClick);
+playBtn.onclick = function () {
+    let fiveMinutes = 60 * 5,
+        display = document.querySelector('#time');
+    startTimer(fiveMinutes, display);
+};
+///End of timer function/////////
+
+
+//set up eventlisteners  
+document.querySelector('#clickable').addEventListener('click', handleClick);
+
+function handleClick(e) {
+    console.log(e.target.tagName);
+}
+
+
+playBtn.addEventListener('click', init);
+
+document.querySelector('#play').addEventListener('click', handleStart);
+
+function handleStart(e) {
+    console.log(e.target.tagName);
+}
+
+function init(e){
+    console.log('working');
+    playBtn.onclick = function () {
+        bottomEls.style.visibility = 'visible';
+        bottomEltwo.style.visibility = 'visible';
+        playBtn.style.visibility = 'hidden';
+        
+      
+    }
+};
+
+// function renderMessage(winner){
+//     if(bottomEltwo === 10){
+//         return 'Wow you r so guud'
+//     } else {
+//         return 'hOw Do yOu LooOoOoOosE!'
+//     }
+// }
+
+// function checkWinner (){
+//     if(score === 10){
+//         return true
+//     } else if (bottomEls === 0){
+//         return false
+//     }
+
+
+//PLAY button needs to start the game.
+
+//player presses play, timer starts and birds start appearing.
+
+
+// I need the birds(buttons) that appear and disappear off the scree
+//incrementally. 
+
+// need an if / else. IF the player clicks bird get point.
+//ELSE if they miss lose point. 
+
+//IF a player gets 10 points before timer runs out..WIN
+//ELSE no 10 points before timer runs out...LOSE
+
+
+    
+
+
+
+
+
